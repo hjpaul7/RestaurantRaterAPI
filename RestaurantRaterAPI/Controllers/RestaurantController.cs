4f0065a0aa5841c92a962e51dd24ea058e0d46cd
Hustin Jeffers <hj.paul7@gmail.com>
2020-08-25 14:48:21 -0400

    post method

---
+++ RestaurantRaterAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,46 @@
+﻿using RestaurantRaterAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RestaurantRaterAPI.Controllers
+{
+    public class RestaurantController : ApiController
+    {
+        RestaurantDdContext _context = new RestaurantDdContext();
+
+        // Create
+        public async Task<IHttpActionResult> PostRestaurant(Restaurant model)
+        {
+            if(model is null)
+            {
+                return BadRequest("Your request body cannot be empty");
+            }
+
+            if(ModelState.IsValid)
+            {
+                _context.Restaurants.Add(model);
+                await _context.SaveChangesAsync();
+
+                return Ok();
+            }
+            return BadRequest(ModelState);
+        }
+
+        // Read
+            // Get by ID
+
+
+            // Get All
+
+        // Update
+
+
+        // Delete
+
+    }
+}
--- RestaurantRaterAPI/Models/Restaurant.cs
+++ RestaurantRaterAPI/Models/Restaurant.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace RestaurantRaterAPI.Models
 {
+    // Restaurant Entity (class that gets stored in database)
     public class Restaurant
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Rating { get; set; }
+
+        public bool isRecommended => Rating > 3.5;
     }
 }

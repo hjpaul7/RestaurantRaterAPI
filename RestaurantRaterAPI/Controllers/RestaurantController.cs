d9769f14a1153054b00a23475af6945ab29abb7a
Hustin Jeffers <hj.paul7@gmail.com>
2020-08-25 15:48:14 -0400

    put method

--- RestaurantRaterAPI/Controllers/RestaurantController.cs
+++ RestaurantRaterAPI/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRaterAPI.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -11,17 +12,18 @@ namespace RestaurantRaterAPI.Controllers
 {
     public class RestaurantController : ApiController
     {
-        RestaurantDdContext _context = new RestaurantDdContext();
+        private readonly RestaurantDdContext _context = new RestaurantDdContext();
 
         // Create
+        [HttpPost]
         public async Task<IHttpActionResult> PostRestaurant(Restaurant model)
         {
-            if(model is null)
+            if (model is null)
             {
                 return BadRequest("Your request body cannot be empty");
             }
 
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 _context.Restaurants.Add(model);
                 await _context.SaveChangesAsync();
@@ -32,13 +34,50 @@ namespace RestaurantRaterAPI.Controllers
         }
 
         // Read
-            // Get by ID
+        // Get by ID
+        public async Task<IHttpActionResult> GetById(int id)
+        {
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
 
+            if (restaurant != null)
+            {
+                return Ok(restaurant);
+            }
+            return NotFound();
+        }
 
-            // Get All
+        // Get All
+        [HttpGet]
+        public async Task<IHttpActionResult> GetAll()
+        {
+            List<Restaurant> restaurants = await _context.Restaurants.ToListAsync();
+            return Ok(restaurants);
+        }
 
         // Update
+        [HttpPut]
+        public async Task<IHttpActionResult> UpdateRestaurant([FromUri]int id, [FromBody]Restaurant updatedRestaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                // Find and update restaurant
+                Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+                if (restaurant != null)
+                {
+                    // Update restaurant
+                    restaurant.Name = updatedRestaurant.Name;
+                    restaurant.Rating = updatedRestaurant.Rating;
 
+                    await _context.SaveChangesAsync();
+
+                    return Ok("Restaurant has been updated");
+                }
+                // Didn't find restaurant
+                return NotFound();
+            }
+            // Return bad request
+            return BadRequest(ModelState);
+        }
 
         // Delete
 
